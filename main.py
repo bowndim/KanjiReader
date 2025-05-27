58c0eefd8472e86d47738741d1b8a321eaa887b9
Andrew Powell <apowell00@gmail.com>
2025-05-27 21:55:45 +0900

    bugfix

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def generate(data: dict, bk: BackgroundTasks):
     #tmp = tempfile.TemporaryDirectory() # auto-deleted on GC
     tmpdir = tempfile.mkdtemp()
     try:
-        epub, html = await make_reader(out_dir=Path(tmpdir), **data)
+        epub, html = await make_reader(out_dir=pathlib.Path(tmpdir), **data)
     except Exception as exc:
         traceback.print_exc() 
         raise HTTPException(400, str(exc))
